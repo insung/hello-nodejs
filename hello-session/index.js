179abb23d58a2bd0fb641275ef8b12defe627a26
insung <imissyoubrad@gmail.com>
2017-07-17 23:06:52 +0900

    session example

--- hello-session/index.js
+++ hello-session/index.js
@@ -1,7 +1,21 @@
 var express = require('express');
-var app = express;
+var session = require('express-session')
+var app = express();
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
+app.get('/count', function(req, res) {
+    if (req.session.count)
+        req.session.count++;
+    else
+        req.session.count = 1;
+
+    res.send('hi session cnt: ' + req.session.count);
+});
 
 app.listen(3000, function() {
     console.log('listen port 3000 for hello session example..');
